c898f3c304a6bb0225889b6cc0c1b89146fda022
ailson11 <sousa.ailson@escola.pr.gov.br>
2025-05-21 11:44:41 -0300

    21/05

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,12 @@
 criaCartao(
     'Programação',
-    'Quais clubes Neymar já passou?',
-    'Neymar passou por um total de 4 clubes diferentes:
-    Santos, Barcelona, Paris Saint Germain, e atualmente Neymar se encontra no clube do Santos'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quantos anos tem Neymar?',
+    'Neymar tem 33 anos de idade.'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quais clubes Neymar já passou?',
+    'Neymar passou por um total de 4 clubes diferentes:
+    Santos, Barcelona, Paris Saint Germain, e atualmente Neymar se encontra no clube do Santos'
 )
